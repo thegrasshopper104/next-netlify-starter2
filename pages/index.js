a0a9376f931ecd50261d44350dccd6521138dbd6
thegrasshopper104 <57704946+thegrasshopper104@users.noreply.github.com>
2024-07-05 18:31:29 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Üstökös, rátóti csikótojás, körtefa, barackvirág, vörös villamos <code>pages/index.js</code>
         </p>
       </main>
 
